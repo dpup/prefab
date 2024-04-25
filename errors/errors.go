a5e4013aeaaa92d5aff53b191c13cad1573c84f1
Daniel Pupius <dan@pupi.us>
2024-04-25 13:44:19 -0700

    build: Add gRPC status and public message to error interface

--- errors/errors.go
+++ errors/errors.go
@@ -1,9 +1,5 @@
 // Package errors is a fork of `github.com/go-errors/errors` that adds support
-// for gRPC status codes and public messages.
-//
-// Original package description:
-//
-// Package errors provides errors that have stack-traces.
+// for gRPC status codes, public messages, as well as stack-traces.
 //
 // This is particularly useful when you want to understand the
 // state of execution when an error was returned unexpectedly.
@@ -18,10 +14,10 @@
 //
 //	import "github.com/go-errors/errors"
 //
-//	var Crashed = errors.Errorf("oh dear")
+//	var Crashed = errors.Errorf("something really bad just happened")
 //
 //	func Crash() error {
-//	    return errors.New(Crashed)
+//	    return errors.NewC(Crashed, codes.Internal).WithPublicMessage("An unknown error occurred")
 //	}
 //
 // This can be called as follows:
@@ -44,17 +40,18 @@
 //	        }
 //	    }
 //	}
-//
-// This package was original written to allow reporting to Bugsnag,
-// but after I found similar packages by Facebook and Dropbox, it
-// was moved to one canonical location so everyone can benefit.
 package errors
 
 import (
 	"bytes"
 	"fmt"
+	"net/http"
 	"reflect"
 	"runtime"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/runtime/protoiface"
 )
 
 // The maximum number of stackframes on any error.
@@ -67,6 +64,18 @@ type Error struct {
 	stack  []uintptr
 	frames []StackFrame
 	prefix string
+
+	// gRPC status code to associate with an error response.
+	code codes.Code
+
+	// Error details which gRPC returns the client
+	details []protoiface.MessageV1
+
+	// HTTP status code to associate with an error response.
+	httpStatusCode int
+
+	// Error message to return to client,
+	publicMessage string
 }
 
 // New makes an Error from the given value. If that value is already an
@@ -74,6 +83,11 @@ type Error struct {
 // fmt.Errorf("%v"). The stacktrace will point to the line of code that
 // called New.
 func New(e interface{}) *Error {
+	return NewC(e, codes.Unknown)
+}
+
+// NewC makes an Error with a status code defined.
+func NewC(e interface{}, code codes.Code) *Error {
 	var err error
 
 	switch e := e.(type) {
@@ -88,6 +102,7 @@ func New(e interface{}) *Error {
 	return &Error{
 		Err:   err,
 		stack: stack[:length],
+		code:  code,
 	}
 }
 
@@ -116,6 +131,7 @@ func Wrap(e interface{}, skip int) *Error {
 	return &Error{
 		Err:   err,
 		stack: stack[:length],
+		code:  codes.Unknown,
 	}
 }
 
@@ -137,11 +153,50 @@ func WrapPrefix(e interface{}, prefix string, skip int) *Error {
 	}
 
 	return &Error{
-		Err:    err.Err,
-		stack:  err.stack,
-		prefix: prefix,
+		Err:            err.Err,
+		stack:          err.stack,
+		code:           err.code,
+		details:        err.details,
+		httpStatusCode: err.httpStatusCode,
+		publicMessage:  err.publicMessage,
+		prefix:         prefix,
 	}
+}
 
+// WithPublicMessage takes an error message and adds a public message to it. If
+// the error is not already an `Error`, it will be wrapped in one.
+func WithPublicMessage(err error, publicMessage string) *Error {
+	if err == nil {
+		return nil
+	}
+	return Wrap(err, 1).WithPublicMessage(publicMessage)
+}
+
+// WithCode takes an error and adds a gRPC status code to it. If the error is
+// not already an `Error`, it will be wrapped in one.
+func WithCode(err error, code codes.Code) *Error {
+	if err == nil {
+		return nil
+	}
+	return Wrap(err, 1).WithCode(code)
+}
+
+// WithHTTPStatusCode takes an error and adds an explicit HTTP status code to
+// it, overriding the HTTP status mapped from the gRPC code.
+func WithHTTPStatusCode(err error, code int) *Error {
+	if err == nil {
+		return nil
+	}
+	return Wrap(err, 1).WithHTTPStatusCode(code)
+}
+
+// WithDetails takes an error and adds gRPC details to it. If the error is
+// not already an `Error`, it will be wrapped in one.
+func WithDetails(err error, details ...protoiface.MessageV1) *Error {
+	if err == nil {
+		return nil
+	}
+	return Wrap(err, 1).WithDetails(details...)
 }
 
 // Errorf creates a new error with the given message. You can use it
@@ -208,7 +263,131 @@ func (err *Error) TypeName() string {
 	return reflect.TypeOf(err.Err).String()
 }
 
-// Return the wrapped error (implements api for As function).
+// Unwrap the error (implements api for As function).
 func (err *Error) Unwrap() error {
 	return err.Err
+}
+
+// Code returns the gRPC status code associated with the error.
+func (err *Error) Code() codes.Code {
+	return err.code
+}
+
+// WithCode sets the gRPC status code associated with the error.
+func (err *Error) WithCode(code codes.Code) *Error {
+	err.code = code
+	return err
+}
+
+// Details returns the gRPC details associated with the error.
+func (err *Error) Details() []protoiface.MessageV1 {
+	return err.details
+}
+
+// WithDetails sets the gRPC details associated with the error.
+func (err *Error) WithDetails(details ...protoiface.MessageV1) *Error {
+	err.details = append(err.details, details...)
+	return err
+}
+
+// HTTPStatusCode returns the HTTP status code that should be returned to the
+// client. If a code is set, it will be used, otherwise a default will be
+// returned based on the gRPC code.
+func (err *Error) HTTPStatusCode() int {
+	if err.httpStatusCode != 0 {
+		return err.httpStatusCode
+	}
+	switch err.code {
+	case codes.OK:
+		return http.StatusOK
+	case codes.InvalidArgument, codes.OutOfRange:
+		return http.StatusBadRequest
+	case codes.DeadlineExceeded:
+		return http.StatusGatewayTimeout
+	case codes.NotFound:
+		return http.StatusNotFound
+	case codes.AlreadyExists:
+		return http.StatusConflict
+	case codes.PermissionDenied:
+		return http.StatusForbidden
+	case codes.Unauthenticated:
+		return http.StatusUnauthorized
+	case codes.ResourceExhausted:
+		return http.StatusTooManyRequests
+	case codes.FailedPrecondition:
+		return http.StatusPreconditionFailed
+	case codes.Unimplemented:
+		return http.StatusNotImplemented
+	case codes.Unavailable:
+		return http.StatusServiceUnavailable
+
+	case codes.Canceled, codes.Unknown, codes.Aborted, codes.Internal, codes.DataLoss:
+		return http.StatusInternalServerError
+	}
+
+	return http.StatusInternalServerError
+}
+
+// WithHTTPStatusCode sets the HTTP status code that should be returned to the
+// client.
+func (err *Error) WithHTTPStatusCode(code int) *Error {
+	err.httpStatusCode = code
+	return err
+}
+
+// PublicMessage returns the error string that should be returned to the client.
+func (err *Error) PublicMessage() string {
+	if err.publicMessage != "" {
+		return err.publicMessage
+	}
+	return err.Error()
+}
+
+// WithPublicMessage sets the error string that should be returned to the client.
+func (err *Error) WithPublicMessage(publicMessage string) *Error {
+	err.publicMessage = publicMessage
+	return err
+}
+
+// GRPCStatus returns a gRPC status object for the error.
+func (err *Error) GRPCStatus() *status.Status {
+	st := status.New(err.Code(), err.PublicMessage())
+	if len(err.details) > 0 {
+		st, _ = st.WithDetails(err.details...)
+	}
+	return st
+}
+
+// Code returns a gRPC status code for an error. If the error is nil, it returns
+// codes.OK. If error exposes a `Code()` method, it is returned. Otherwise
+// codes.Internal is returned.
+func Code(err error) codes.Code {
+	if err == nil {
+		return codes.OK
+	}
+	if e, ok := err.(codedError); ok {
+		return e.Code()
+	}
+	return codes.Unknown
+}
+
+// HTTPStatusCode returns an HTTP status code for an error. If the error is nil,
+// it returns http.StatusOK. If error exposes a `HTTPStatusCode()` method, it is
+// returned. Otherwise http.StatusInternalServerError is returned.
+func HTTPStatusCode(err error) int {
+	if err == nil {
+		return http.StatusOK
+	}
+	if e, ok := err.(httpError); ok {
+		return e.HTTPStatusCode()
+	}
+	return http.StatusInternalServerError
+}
+
+type codedError interface {
+	Code() codes.Code
+}
+
+type httpError interface {
+	HTTPStatusCode() int
 }
